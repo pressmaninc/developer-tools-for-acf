b9a1fa66a3c6e91c4d91403fd2598152203b7067
anahara <kazunao_anahara@pressman.ne.jp>
2020-04-06 22:16:09 +0900

    fixed a bug. error when the label was undefined.

--- assets/js/developer-tools-for-acf-field.js
+++ assets/js/developer-tools-for-acf-field.js
@@ -1,34 +1,34 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 
 	var PmDebugToolForAcf = {
 		list_of_fields: {},
 		done_field: {},
 		hide_items: [],
 		last_postbox_container: $('.postbox-container:last-child'),
-		make_feild_detail_html : function(field_name, field_key, field_type){
+		make_feild_detail_html: function (field_name, field_key, field_type) {
 			return '<span class="pm-acf-field-detail">'
 				+ '<span class="pm-acf-field field_name"><em>name</em>' + field_name + '</span>'
 				+ '<span class="pm-acf-field field_key"><em>key</em>' + field_key + '</span>'
 				+ '<span class="pm-acf-field field_type"><em>type</em>' + field_type + '</span>'
 				+ '</span>';
 		},
-		get_description_html : function(label){
-			return ( $(label.find('p.description')).html() != undefined ) ? $(label.find('p.description'))[0].outerHTML : '';
+		get_description_html: function (label) {
+			return ($(label.find('p.description')).html() != undefined) ? $(label.find('p.description'))[0].outerHTML : '';
 		},
-		get_required_html : function(label){
-			return ( $(label.find('span.acf-required')).html() != undefined ) ? $(label.find('span.acf-required'))[0].outerHTML : '';
+		get_required_html: function (label) {
+			return ($(label.find('span.acf-required')).html() != undefined) ? $(label.find('span.acf-required'))[0].outerHTML : '';
 		},
-		make_list : function(list){
+		make_list: function (list) {
 			var buf = '';
-			$.each( list, function( panel, detail ) {
+			$.each(list, function (panel, detail) {
 				buf += '<div class="panel table">'
 					+ '<h3>' + panel + '</h3>'
 					+ '<div class="table">';
-				$.each( detail, function( i, item ) {
+				$.each(detail, function (i, item) {
 					buf += '<div class="tr">'
-							+ '<span class="td label">' + item[0] + '</span>'
-							+ '<span class="td detail">' + item[1] + '</span>'
-							+ '</div>';
+						+ '<span class="td label">' + item[0] + '</span>'
+						+ '<span class="td detail">' + item[1] + '</span>'
+						+ '</div>';
 				});
 				buf += '</div>'
 					+ '</div>';
@@ -40,15 +40,15 @@ jQuery(document).ready(function($){
 	if (typeof acf !== 'undefined' && typeof dtfa_settings !== 'undefined') {
 
 		// get ignore type
-		PmDebugToolForAcf.ignore_field_type = ( dtfa_settings['ignore_field_type'] ) ? dtfa_settings['ignore_field_type'] : [];
+		PmDebugToolForAcf.ignore_field_type = (dtfa_settings['ignore_field_type']) ? dtfa_settings['ignore_field_type'] : [];
 
 		// get hide items
-		PmDebugToolForAcf.hide_items = ( dtfa_settings['hide_field_detail'] ) ? dtfa_settings['hide_field_detail'] : [];
+		PmDebugToolForAcf.hide_items = (dtfa_settings['hide_field_detail']) ? dtfa_settings['hide_field_detail'] : [];
 
 		// display details to field label
-		$(".acf-field").each(function(i) {
+		$(".acf-field").each(function (i) {
 
-			if( $.inArray($(this).attr('data-type'), PmDebugToolForAcf.ignore_field_type) === -1 ){
+			if ($.inArray($(this).attr('data-type'), PmDebugToolForAcf.ignore_field_type) === -1) {
 
 				var field = $(this);
 				var group_name = field.closest('.acf-postbox').find('h2 span').text();
@@ -63,12 +63,12 @@ jQuery(document).ready(function($){
 				var required = '';
 
 				// prevent run twice
-				if( PmDebugToolForAcf.done_field[field_key] == undefined || PmDebugToolForAcf.done_field[field_key] != 1 ){
+				if (PmDebugToolForAcf.done_field[field_key] == undefined || PmDebugToolForAcf.done_field[field_key] != 1) {
 
 					// find where label is
-					if( label_html == undefined ){
-						if( $( field.parents('table').find('th[data-key=' + field_key + ']')[0] ) != undefined ){
-							label = $( field.parents('table').find('th[data-key=' + field_key + ']')[0] );
+					if (label_html == undefined) {
+						if ($(field.parents('table').find('th[data-key=' + field_key + ']')[0]) != undefined) {
+							label = $(field.parents('table').find('th[data-key=' + field_key + ']')[0]);
 							label_html = label.html();
 							found_label = true;
 						}
@@ -77,21 +77,23 @@ jQuery(document).ready(function($){
 						found_label = true;
 					}
 
-					if( found_label == true ){
+					if (found_label == true) {
 						description = PmDebugToolForAcf.get_description_html(label);
 						required = PmDebugToolForAcf.get_required_html(label);
-						label_html = label_html.replace(description, '').replace(required, '');
+						if (label_html != undefined) {
+							label_html = label_html.replace(description, '').replace(required, '');
+						}
 						label_text = '<span class="label_only">' + $('<p>' + label_html + '</p>').text() + required + '</span>';
 						var feild_detail = PmDebugToolForAcf.make_feild_detail_html(field_name, field_key, field_type);
 
 						// replace label
-						label.html( label_text + feild_detail + description );
+						label.html(label_text + feild_detail + description);
 
 						// keep for list
-						if (PmDebugToolForAcf.list_of_fields[group_name] == undefined ){
+						if (PmDebugToolForAcf.list_of_fields[group_name] == undefined) {
 							PmDebugToolForAcf.list_of_fields[group_name] = [];
 						}
-						PmDebugToolForAcf.list_of_fields[group_name].push([ label_text, feild_detail ]);
+						PmDebugToolForAcf.list_of_fields[group_name].push([label_text, feild_detail]);
 
 						// prevent run twice
 						PmDebugToolForAcf.done_field[field_key] = 1;
@@ -103,13 +105,13 @@ jQuery(document).ready(function($){
 		});
 
 		// remove hide items
-		if(PmDebugToolForAcf.hide_items.length === 3){
+		if (PmDebugToolForAcf.hide_items.length === 3) {
 			// remove all
 			$('.pm-acf-field-detail').remove()
 		} else {
 			// remove partial
-			$.each( PmDebugToolForAcf.hide_items, function( i, item ) {
-				if( item != undefined ){
+			$.each(PmDebugToolForAcf.hide_items, function (i, item) {
+				if (item != undefined) {
 					$('.pm-acf-field.field_' + item).remove();
 				}
 			});
